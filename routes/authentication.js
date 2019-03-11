356ab94126ce498b4c4cc1967afc5261fa99aae2
Timothy Sealy <timothy.sealy@gmail.com>
2019-03-11 22:42:08 +0100

    Added apidoc to authenticate endpoint

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,6 +4,21 @@ let router  = require('express').Router()
 
 var User = require('../models/User')
 
+/**
+ * @api {post} /api/authenticate Authenticate a user
+ * @apiGroup Authentication
+ * @apiSuccess {Message} message A message containing a JWT token.
+ * @apiSuccessExample {json} Success
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "success": true,
+ *    "message": "Authentication succesful.",
+ *    "token": "eyJhbGciOiJIUzI1...",
+ *    "expiresIn": 1440
+ *   }
+ * @apiErrorExample {json} Query error
+ *    HTTP/1.1 500 Internal Server Error
+ */
 router.post('/', function (req, res) {
   User.findOne({
     name: req.body.username
