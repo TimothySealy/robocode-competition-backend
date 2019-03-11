b917f2b5711d47aba6f655a10ea680c548123a08
Timothy Sealy <timothy.sealy@gmail.com>
2019-03-11 23:58:11 +0100

    Added endpoint for retrieving the teams (including their secret keys). Endpoint (/api/teams/all) is not REST compliant but it will have to do for now.

--- routes/teams.js
+++ routes/teams.js
@@ -46,4 +46,56 @@ router.get('/', function (req, res) {
   })
 })
 
+/***
+ *
+ * Protected endpoints below.
+ *
+ ***/
+router.use(require('./api_authorization'))
+
+/**
+ * @api {get} /api/teams/all List all teams (including secrets)
+ * @apiGroup Teams
+ * @apiHeader {String} x-authentication The JWT access token.
+ *
+ * @apiSuccess {Boolean} succes A boolean indicating whether the request was succesful.
+ * @apiSuccess {String} message Error or succes message.
+ * @apiSuccess {Teams[]} teams A list of teams.
+ * @apiSuccessExample {json} Success
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "success": true,
+ *    "message": "Teams succesfully retrieved",
+ *    "teams": [{
+ *      "code": "nl.saxion.ehi1vsa1",
+ *      "name": "ehi1vsa1",
+ *      "logo": "teamlogo_default.png",
+ *      "competitions": ["useb_2019"]
+ *    },
+ *    {
+ *      ...
+ *    }]
+ *  }
+ * @apiErrorExample {json} Query error
+ *    HTTP/1.1 500 Internal Server Error
+ */
+router.get('/all', function (req, res) {
+  let where = {}
+  let fields = {}
+  Team.find(where, fields, function (err, teams) {
+    if (err) {
+      console.error(err);
+      res.status(500).json({
+        success: false,
+        message: "Cannot find teams"
+      })
+    }
+    res.status(200).json({
+      success: true,
+      message: "Teams succesfully retrieved",
+      teams: teams
+    })
+  })
+})
+
 module.exports = router
